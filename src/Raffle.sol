2e19345e5550333866149276445a2b1d9f3212de
Patrick Pekel <pekelpatrick@gmail.com>
2026-09-20 19:17:29 +0200

    fix fmt issues

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -25,7 +25,9 @@
 pragma solidity 0.8.19;
 
 import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
-import {VRFV2PlusClient} from "../lib/chainlink-brownie-contracts/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
+import {
+    VRFV2PlusClient
+} from "../lib/chainlink-brownie-contracts/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
 
 /**
  * @title Raffle contract
